81c0d8c1d27f06a52d8127a0c0cdbf8fc2e8951a
Henry Nawrocki <hdev@sztanski.com>
2022-05-03 11:51:06 -0500

    Change reducer names to be semantical

--- src/components/book-list/BookList.jsx
+++ src/components/book-list/BookList.jsx
@@ -1,9 +1,14 @@
+import { useSelector } from 'react-redux';
 import Book from '../book/Book';
 
-const BookList = () => (
-  <div className="bookscontainer">
-    <Book title="The Davinci Code" author="Dan Brown" />
-  </div>
-);
+const BookList = () => {
+  const books = useSelector((state) => state);
+  console.log(books);
+  return (
+    <div className="bookscontainer">
+      <Book title="The Davinci Code" author="Dan Brown" />
+    </div>
+  );
+};
 
 export default BookList;
