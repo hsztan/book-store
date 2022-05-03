47520486352f139e14b5f7a8a40c886bb2f4e1a0
Henry Nawrocki <hdev@sztanski.com>
2022-05-03 11:24:26 -0500

    Add initial book test data

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 const ADD_BOOK = 'bookstore/book/ADD_BOOK';
 const DELETE_BOOK = 'bookstore/book/DELETE_BOOK';
 
-const initialState = [];
+const initialState = [
+  { title: 'The Da Vinci Code', author: 'Dan Brown', id: 1 },
+  { title: 'Catcher in the Rye', author: 'J.D. Salinger', id: 2 },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', id: 3 },
+  { title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', id: 4 },
+];
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
