2dd9578aaf9fab0601c070a0a878934e98de49a5
Henry Nawrocki <hdev@sztanski.com>
2022-05-05 13:45:07 -0500

    Remove header container

--- src/components/book-list/BookList.jsx
+++ src/components/book-list/BookList.jsx
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { getBooks } from '../../redux/books/books';
 import Book from '../book/Book';
+import './BookList.style.scss';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
@@ -13,9 +14,11 @@ const BookList = () => {
 
   return (
     <div className="bookscontainer">
-      {books.map((book) => (
-        <Book key={book.item_id} book={book} />
-      ))}
+      <ul className="booklist">
+        {books.map((book) => (
+          <Book key={book.item_id} book={book} />
+        ))}
+      </ul>
     </div>
   );
 };
